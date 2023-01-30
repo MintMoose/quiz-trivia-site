825ce460f200c8abe8ee08b3b837e6678706af5f
FunkyChicken64 <riqab@hotmail.co.uk>
2023-01-30 14:25:50 +0000

    removed hard coded number.

--- src/Components/QuestionPage.js
+++ src/Components/QuestionPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import SingleQuestion from "./SingleQuestion.js";
 
 export default function QuestionPage(props) {
-  const fiveQuestion = props.question.map((element) => {
+  const questionList = props.question.map((element) => {
     return (
       <SingleQuestion
         key={element.id}
@@ -16,10 +16,13 @@ export default function QuestionPage(props) {
   return (
     <div className="entire-div">
       <div className="questionpage-div">
-        <div className={props.done ? "disabled" : ""}>{fiveQuestion}</div>
+        <div className={props.done ? "disabled" : ""}>{questionList}</div>
         <div className="inline-button">
           {props.done && (
-            <span>You scored {props.numCorrect}/5 correct answers</span>
+            <span>
+              You scored {props.numCorrect}/{questionList.length} correct
+              answers
+            </span>
           )}
           <button onClick={props.checkAnswers} className="submit--button">
             {props.done ? "New Game" : "Check answers"}
