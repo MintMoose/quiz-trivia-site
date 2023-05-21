89d2abb53003cf331564bb0b8df99a18a6d8ae07
FunkyChicken64 <riqab@hotmail.co.uk>
2023-05-21 13:59:59 +0100

    remove old test

--- tests/components/SingleQuestion.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen, cleanup } from "@testing-library/react";
-import SingleQuestion from "../../Components/SingleQuestion";
-
-test("Should render SingleQuestion Componenet", () => {
-  render(<SingleQuestion />);
-  const singleQuestionElement = screen.getByTestId("single-question");
-  expect(singleQuestionElement).toBeInTheDocument();
-});
